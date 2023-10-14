22e6e48097ba45ab02673bea791a3827a39c4a7e
Anuga Mindinu <99359730+anugamindinu@users.noreply.github.com>
2023-10-14 13:16:49 +0530

    comited all

---
+++ MAD Project/app/src/main/java/com/example/user_profile/Edit_store.kt
@@ -0,0 +1,11 @@
+package com.example.user_profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Edit_store : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_store)
+    }
+}
---
+++ MAD Project/app/src/main/java/com/example/user_profile/user_store_viwe.kt
@@ -0,0 +1,11 @@
+package com.example.user_profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class user_store_viwe : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_store_viwe)
+    }
+}
